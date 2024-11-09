058dda66a90212d5028642f74a3d0167248123b9
Nick Weedon <nick@weedon.org.au>
2024-11-09 18:01:45 -0500

    Increased Z axis movement speed

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -2130,7 +2130,7 @@
 #endif
 
 // Homing speeds (linear=mm/min, rotational=°/min)
-#define HOMING_FEEDRATE_MM_M { (80*60), (80*60), (4*60) }
+#define HOMING_FEEDRATE_MM_M { (80*60), (80*60), (15*60) }
 
 // Validate that endstops are triggered on homing moves
 #define VALIDATE_HOMING_ENDSTOPS
